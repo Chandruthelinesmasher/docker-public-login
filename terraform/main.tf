c492f1681ce89599f917d55cb83031c49f50dd07
chandru <kumaravelchandru982@gmail.com>
2026-02-10 23:01:26 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,8 +34,8 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = try(module.eks.cluster_endpoint, "")
+  cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -44,7 +44,7 @@ provider "kubernetes" {
       "eks",
       "get-token",
       "--cluster-name",
-      module.eks.cluster_name,
+      try(module.eks.cluster_name, var.cluster_name),
       "--region",
       var.aws_region
     ]
